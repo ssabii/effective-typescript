1cb4ca72a9d3d06bbc9e1ace1a5468b87d4cefe5
ssabii <fhy1930@gmail.com>
2023-06-04 15:42:24 +0900

    feat: add ch1 item5

---
+++ src/ch1/item5.ts
@@ -0,0 +1,10 @@
+let age: number;
+age = '12'; // 오류
+age = '12' as any; // 정상
+
+function calculateAge(birthDate: Date) {
+  return Date.now() - birthDate.getTime();
+}
+
+let birthDate: any = '1990-01-19';
+calculateAge(birthDate); // 정상
