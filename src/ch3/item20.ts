e939e39601affa961e6843bf44f4ca0d9b2e5449
ssabii <fhy1930@gmail.com>
2023-06-15 16:47:22 +0900

    feat: add ch3 item20

---
+++ src/ch3/item20.ts
@@ -0,0 +1,16 @@
+// 다른 타입에는 다른 변수 사용하기
+namespace Item20 {
+  let id = '12-34-56'; // string으로 사용
+  id = 123456;
+  // '123456' 형식은 'string' 형식에 할당할 수 없습니다.
+
+  let id: string | number = '12-34-56';
+  id = 123456; // 정상
+
+  // 다른 타입을 사용할 경우 별도의 변수를 사용하는 것이 좋다.
+  const id = '12-34-56';
+  fetchProduct(id);
+
+  const serial = 123456; // 정상
+  fetchProductBySerialNumber(serial); // 정상
+}
